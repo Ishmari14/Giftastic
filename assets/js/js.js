f493d120500270d01fb6ed718d6f724071e1de62
Jhayla Young <jhayla.young@gmail.com>
2019-10-27 19:05:51 -0500

    working on it

---
+++ assets/js/js.js
@@ -0,0 +1,15 @@
+///Starting Anime Variable///
+
+var anime = [
+    'One Piece',
+    'Hunter x Hunter',
+    'My Hero Academia',
+    'Symphogear',
+    'Sailor Moon',
+    'Megalo Box',
+    'Naruto',
+    'Bleach',
+    'Dr.Stone',
+    'Demon Slayer'
+];
+
