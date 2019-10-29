f339d63ceacd8fee073bf78665c6cd5b2153c487
Jhayla Young <jhayla.young@gmail.com>
2019-10-28 21:33:51 -0500

    working on getting gifs to generate

--- assets/js/js.js
+++ assets/js/js.js
@@ -32,3 +32,34 @@ $(document).ready(function () {
     }
 
 });
+
+function animeDisplay() {
+    $("button").on("click", function () {
+        var anime = $(this).attr("anime-buttons");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            anime + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+
+                    var animeImage = $("<img>");
+                    animeImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(animeImage);
+
+                    $("#anime-gifs").prepend(gifDiv);
+
+                    console.log("working gifs");
+                }
+            });
+    });
+}
+
